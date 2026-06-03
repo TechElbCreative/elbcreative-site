375b80c5751e7580b3f6dd96e549a0d174350319
Dezmond Norman <hakai@Dezmonds-MacBook-Air.local>
2026-06-03 11:41:13 -0400

    Fix OAuth callback postMessage format for Decap

--- functions/callback.js
+++ functions/callback.js
@@ -22,20 +22,35 @@ export async function onRequest(context) {
 
   const data = await response.json();
   const token = data.access_token;
+  const provider = 'github';
 
-  const script = `
-    <script>
-      (function() {
-        window.opener.postMessage(
-          'authorization:github:success:' + JSON.stringify({token: '${token}', provider: 'github'}),
-          'https://elbcreative.co'
-        );
-        window.close();
-      })();
-    <\/script>
-  `;
-
-  return new Response(`<!DOCTYPE html><html><body>${script}</body></html>`, {
+  return new Response(`<!DOCTYPE html>
+<html>
+<body>
+<script>
+(function() {
+  var data = {
+    token: "${token}",
+    provider: "${provider}"
+  };
+  var message = "authorization:" + data.provider + ":success:" + JSON.stringify(data);
+  function sendMessage() {
+    if (window.opener) {
+      window.opener.postMessage(message, document.location.origin);
+      window.opener.postMessage(message, 'https://elbcreative.co');
+      setTimeout(function() { window.close(); }, 1000);
+    }
+  }
+  if (document.readyState === 'complete') {
+    sendMessage();
+  } else {
+    window.addEventListener('load', sendMessage);
+  }
+})();
+<\/script>
+<p>Authenticating... you can close this window.</p>
+</body>
+</html>`, {
     headers: { 'Content-Type': 'text/html' }
   });
 }
